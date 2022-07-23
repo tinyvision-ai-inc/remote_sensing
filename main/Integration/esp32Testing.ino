729bfc4c8f94f6868ef3408b60dc22c66120460a
Eesh Vij <45907510+Dat-Bois@users.noreply.github.com>
2022-07-23 10:02:13 -0700

    Added RFID/NFC integration

--- main/Integration/esp32Testing.ino
+++ main/Integration/esp32Testing.ino
@@ -5,6 +5,10 @@
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#include "time.h"
 
 // Wifi credentials
 const char *WIFI_SSID = "VIJ-2";
@@ -19,10 +23,20 @@ String hostname = "TinyESP32";
 #define AWS_IOT_TOPIC_LOCK DEVICE_NAME "/lock"
 #define AWS_MAX_RECONNECT_TRIES 50
 
-bool lockEngage = true;
+#define SS_PIN  21  
+#define RST_PIN 27   
+#define LED_PIN 13 
+
+const char* ntpServer = "pool.ntp.org";
+
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 WiFiClientSecure net = WiFiClientSecure();
 MQTTClient client = MQTTClient(256);
+
+bool lockEngage = true;
+
+String masterID = "A34B2B13";
 
 void connectToWiFi()
 {
@@ -33,8 +47,10 @@ void connectToWiFi()
 
   int retries = 0;
   while (WiFi.status() != WL_CONNECTED && retries < 15){
+    digitalWrite(LED_PIN, HIGH);
     delay(500);
     Serial.print(".");
+    digitalWrite(LED_PIN, LOW);
     retries++;
   }
 
@@ -63,7 +79,9 @@ void connectToAWS()
 
   while (!client.connect(DEVICE_NAME) && retries < AWS_MAX_RECONNECT_TRIES) {
     Serial.print(".");
+    digitalWrite(LED_PIN, HIGH);
     delay(100);
+    digitalWrite(LED_PIN, LOW);
     retries++;
   }
   if(!client.connected()){
@@ -75,6 +93,17 @@ void connectToAWS()
   client.subscribe(AWS_IOT_TOPIC_LOCK);
 }
 
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
 void sendJsonToAWS()
 {
   StaticJsonDocument<128> jsonDoc;
@@ -82,11 +111,12 @@ void sendJsonToAWS()
   JsonObject reportedObj = stateObj.createNestedObject("reported");
   long randNumber;
   
-  reportedObj["temperature"] = random(-40, 100);
-  reportedObj["GPS_Coordinates"] = "41.40338, 2.17403";
-  reportedObj["BatteryLevel"] = "79%";
+  reportedObj["temperature"] = random(40, 100);
+  //reportedObj["GPS_Coordinates"] = "41.40338, 2.17403";
+  //reportedObj["BatteryLevel"] = "79%";
   reportedObj["wifi_strength"] = WiFi.RSSI();
-  reportedObj["Lock Status"] = lockEngage;
+  reportedObj["LockStatus"] = boolToString(lockEngage);
+  reportedObj["timestamp"] = getTime();
   
   //JsonObject locationObj = reportedObj.createNestedObject("location");
   //locationObj["name"] = "Garden";
@@ -95,7 +125,7 @@ void sendJsonToAWS()
   //serializeJson(doc, Serial);
   char jsonBuffer[512];
   serializeJson(jsonDoc, jsonBuffer);
-  
+  //Serial.println(jsonBuffer);
   client.publish(AWS_IOT_TOPIC_PUB, jsonBuffer);
   client.publish(AWS_IOT_TOPIC_SHADOW, jsonBuffer);
 }
@@ -116,28 +146,64 @@ String boolToString(bool val) {
   String converted;
   if(val){
     converted = "True";
+    digitalWrite(LED_PIN, HIGH);
   }
   else {
     converted = "False";
+    digitalWrite(LED_PIN, LOW);
   }
   return converted;
 }
 
+void NFC() {
+  String tagID = "";
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        //Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        //Serial.print(rfid.uid.uidByte[i], HEX);
+        tagID.concat(String(rfid.uid.uidByte[i], HEX));
+      }
+      tagID.toUpperCase();
+      Serial.println(tagID);
+      if(tagID == masterID) {
+        lockEngage = false;
+      }
+      else {
+        lockEngage = true;
+      }
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+      sendJsonToAWS();
+    }
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
   connectToWiFi();
   connectToAWS();
   client.onMessage(messageReceived);
-  randomSeed(analogRead(A9));
+  configTime(0, 0, ntpServer);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+  randomSeed(analogRead(A7));
 }
 
 void loop() {
   if (!client.connected()) {
     connectToAWS();
   }
-  sendJsonToAWS();
   client.loop();
-  delay(1000);
+  NFC();
+  delay(100);
   Serial.println(boolToString(lockEngage));
-
 }
